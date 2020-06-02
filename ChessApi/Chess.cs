7e0dc0d063d8a8c6208e0d68cf97760a36b1a871
Ирхин Илья <i.irhin@yandex.ru>
2020-06-02 15:41:02 +0300

    Добавьте файлы проекта.

---
+++ ChessApi/Chess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessApi
+{
+    public class Chess
+    {
+        public string fen { get; private set; }
+        public Chess(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")//конструктор с начальной позицией по умолчанию
+        {
+            this.fen = fen;
+
+        }
+
+        public Chess Move(string move)//функция позволяющая делать ход Pe2e4(пешка переместилась с e2 на e4) Pe7e8Q(Q - это в кого она превратилась)
+        {
+            Chess nextChess = new Chess(fen);
+            return nextChess;
+        }
+        public char GetFigureAt(int x, int y)//получить где находится фигура
+        {
+            return '.';
+        }
+    }
+}
---
+++ DemoChess/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessApi;
+
+namespace DemoChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chess chess = new Chess();
+            while (true)
+            {
+                Console.WriteLine(chess.fen);
+                string move = Console.ReadLine();
+                if (move == "") break;
+                chess = chess.Move(move);
+
+            }
+        }
+    }
+}
